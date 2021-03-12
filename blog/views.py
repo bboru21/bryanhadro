b2e1464607d4b04d2cf60bf9f0bd472d9ce3347a
bboru21 <bryan.e.hadro@gmail.com>
2021-03-12 20:22:27 +0000

    adds blog posts to view

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
+
+    posts = Post.objects.all()
     context = {
         'title': 'BryanHadro.com - Blog',
+        'posts': posts,
     }
     return render(request, 'blog/index.html', context)

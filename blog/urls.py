5ff383c54df49097ec5a263faf552039aa004c0c
bboru21 <bryan.e.hadro@gmail.com>
2021-08-02 13:19:19 -0400

    adds trailing slash for blog post url

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='blog_index'),
-    re_path(r'(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<day>[\d]+)/(?P<urlname>[a-zA-Z0-9\-]+)$', views.post, name='blog_post')
+    re_path(r'(?P<year>[\d]+)/(?P<month>[\d]+)/(?P<day>[\d]+)/(?P<urlname>[a-zA-Z0-9\-]+)/?$', views.post, name='blog_post')
 ]

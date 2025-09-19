cc99543790fe1a4e440460445fc5070cc36a3013
Bryan Hadro <bryan@merchbar.com>
2025-09-19 15:51:05 -0400

    adds favicon redirects

--- website/urls.py
+++ website/urls.py
@@ -22,8 +22,31 @@ from django.urls import (
     path,
     re_path,
 )
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 from . import views
+
+favicon_files = [
+    'favicon-57.png',
+    'favicon-120.png',
+    'favicon-152.png',
+    'favicon-167.png',
+    'favicon-180.png',
+    'favicon-192.png',
+    'favicon-512.png',
+    'favicon-813.png',
+    'favicon.ico',
+    'favicon.png',
+]
+
+favicon_patterns = [
+    path(
+        filename,
+        RedirectView.as_view(url=staticfiles_storage.url(f'favicons/{filename}'))
+    )
+    for filename in favicon_files
+]
 
 urlpatterns = [
     path('', views.index, name='homepage'),
@@ -33,9 +56,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
+
+    *favicon_patterns,
 ]
 
 if settings.DEBUG:
     urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-

08414eb414da4c0450832c2fb20117d7bc458a15
Bryan Hadro <bryan@merchbar.com>
2025-09-30 14:04:25 -0400

    updates favicons

--- website/urls.py
+++ website/urls.py
@@ -28,14 +28,19 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from . import views
 
 favicon_files = [
-    'favicon-57.png',
-    'favicon-120.png',
-    'favicon-152.png',
-    'favicon-167.png',
-    'favicon-180.png',
-    'favicon-192.png',
-    'favicon-512.png',
-    'favicon-813.png',
+    'android-chrome-192x192.png',
+    'android-chrome-512x512.png',
+    'apple-touch-icon.png',
+    'favicon-16x16.png',
+    'favicon-32x32.png',
+    'favicon-57x57.png',
+    'favicon-120x120.png',
+    'favicon-152x152.png',
+    'favicon-167x167.png',
+    'favicon-180x180.png',
+    'favicon-192x192.png',
+    'favicon-512x512.png',
+    'favicon-813x813.png',
     'favicon.ico',
     'favicon.png',
 ]
